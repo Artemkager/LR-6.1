e9ca1e5e2deb544eec0b2447c8e5e3737e75363d
airex <airex@DESKTOP-PAB22CS>
2021-06-27 20:22:56 +0300

    Добавьте файлы проекта.

---
+++ LR 6.1/LR 6.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void max()
+{
+	double a, b, c;
+	cout << "1-ое число - ";
+	cin >> a;
+	cout << "2-ое число - ";
+	cin >> b;
+	cout << "3-ее число - ";
+	cin >> c;
+	if (a > b && a > c)
+	{
+		cout << a << " - число a больше";
+	}
+	else if (b > a && b > c)
+	{
+		cout << b << " - число b больше";
+	}
+	else (c > a && c > b); {
+		cout << c << " – максимальное число";
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	max();
+	cout << endl;
+	system("pause");
+	return 0;
+}
